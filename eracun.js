0928613f3ad1978a2d12cd95dd0361c0e7968139
Ivan Antešić <ia6382@student.uni-lj.si>
2016-05-07 10:36:13 +0000

    prototip 1.naloge

--- eracun.js
+++ eracun.js
@@ -195,6 +195,7 @@ var vrniRacune = function(callback) {
   );
 }
 
+var sporociloTekst;
 // Registracija novega uporabnika
 streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
@@ -209,12 +210,18 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
       //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
+    if(napaka2 == true){
+      sporociloTekst = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+    }
+    else{
+      sporociloTekst = "Stranka je bila uspešno registrirana.";
+    }
+    odgovor.redirect("/prijava");
     odgovor.end();
   });
 })
@@ -223,7 +230,7 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: sporociloTekst, seznamStrank: stranke, seznamRacunov: racuni});  
       }) 
     });
 })
